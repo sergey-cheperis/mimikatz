b6c1030e3500c5a7d43662db857d061048d71afd
Sergey Cheperis <czz@czz.name>
2020-09-04 05:48:40 -0700

    Remove built-in sqlite3, so external one can be used; move sqlite3.h to inc/

--- mimikatz/modules/dpapi/packages/kuhl_m_dpapi_chrome.h
+++ mimikatz/modules/dpapi/packages/kuhl_m_dpapi_chrome.h
@@ -5,7 +5,7 @@
 */
 #pragma once
 #include "../kuhl_m_dpapi.h"
-#include "../modules/sqlite3.h"
+#include "../inc/sqlite3.h"
 
 NTSTATUS kuhl_m_dpapi_chrome(int argc, wchar_t * argv[]);
 BOOL kuhl_m_dpapi_chrome_isTableExist(sqlite3 *pDb, const char *table);
